2f143107e462b8ffd049f7c0c8e02d7e9f877914
Jaeseong <59774709+codejaeseong@users.noreply.github.com>
2020-08-11 04:55:46 +0900

    Update MouseLoock.cs
    comment

--- MouseLoock.cs
+++ MouseLoock.cs
@@ -24,7 +24,7 @@ public class MouseLook : MonoBehaviour
         float mouseY = touchField.touchDist.y * mouseSensitivity * Time.deltaTime;
 
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90, 90f);
+        xRotation = Mathf.Clamp(xRotation, -90, 90f);   // block the flip screen
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
